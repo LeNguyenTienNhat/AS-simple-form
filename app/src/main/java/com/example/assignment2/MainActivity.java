50f542e7e199572c93fc009fb534304e00e41f71
Le Nguyen Tien Nhat <xlenhat1602x@gmail.com>
2024-05-21 16:26:01 +0700

    final fix

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.assignment2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.instagram);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.instagram_login_screen), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Bind layout
+        setContentView(R.layout.facebook);
+
+        //Bind view (element in HTML)
+        Button signInBtn = (Button) findViewById(R.id.signInBtn);
+
+        //Set event on click for button
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.onSignIn(v);
+            }
         });
     }
+
+    public void onSignIn(View v) {
+        String rightPassword = "123456";
+        String username = "admin";
+
+        //Get EditText content
+        EditText passwordInputField = (EditText) findViewById(R.id.password);
+        String inputtedPassword = (String) passwordInputField.getText().toString();
+
+        //Business logic
+        if (rightPassword.equals(inputtedPassword)) {
+            Toast.makeText(MainActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+    
 }
